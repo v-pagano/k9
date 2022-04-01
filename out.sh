1bf4681c6454f09e3a6e6ceee11a7f34192237bd
v-pagano <vpagano@tgen.org>
2022-04-01 12:53:38 -0700

    Increased ubam support

--- out.sh
+++
@@ -1,7 +0,0 @@
-nextflow run https://github.com/v-pagano/k9 \
-		 -name ecstatic_hamilton \
-		 -params-file nf-1Xq2lSJgvuy3VQ.params.json \
-		 -with-tower http://pnap-tower.tgen.org:8000/api \
-		 -r master \
-		 -profile dback \
-		 -latest
